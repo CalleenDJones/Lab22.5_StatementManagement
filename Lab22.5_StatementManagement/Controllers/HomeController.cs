dd97492f449abade357117a7cb6e51b217d9cbe3
Calleen D. Jones <calleendjones@gmail.com>
2018-05-24 02:20:42 -0400

    First Commit

---
+++ Lab22.5_StatementManagement/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab22._5_StatementManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            if (Session["Counter"] == null)
+            {
+                Session.Add("Counter", 0);
+
+            }
+            int visits = (int)Session["Counter"];
+            visits++;
+
+            Session["Counter"] = visits;
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+            public ActionResult Welcome
+            (string userName = "Anyname", string colorChoice = "Gold", string passWord1 = "password")
+        {
+            //ActionResults
+            ViewBag.UserName = userName;
+            ViewBag.ColorChoice = colorChoice;
+            ViewBag.Password1 = passWord1;
+            
+            return View();
+        }
+    }
+}
---
+++ Lab22.5_StatementManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab22._5_StatementManagement.MvcApplication" Language="C#" %>
